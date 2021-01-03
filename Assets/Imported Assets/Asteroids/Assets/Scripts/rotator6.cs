07f1605f8bf648c17b9d41afe67bb8e132032611
Justin Downs <justin.downs@knights.ucf.edu>
2021-01-03 14:30:16 -0500

    pre Version Swap

---
+++ Assets/Imported Assets/Asteroids/Assets/Scripts/rotator6.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator6 : MonoBehaviour
+{
+    void Update()
+    {
+        GetComponent<Rigidbody>().transform.Rotate(0.1f, 0.0f, 0.0f, Space.Self);
+    }
+}
